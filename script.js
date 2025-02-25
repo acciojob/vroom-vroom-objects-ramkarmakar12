745054421aae42eee8098d118aba50ef1b55e20d
Ramkrishna Karmakar <53562477+ramkarmakar12@users.noreply.github.com>
2025-02-25 08:42:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
-// Complete the js code
-function Car(make, model) {}
+//complete this code
+class Person {
+	constructor(name,age)
+	{
+			this._name = name;
+			this._age = age;
+	}
+	get name(){
+		return this._name;
+	}
+	set name(name){
+		this._name = name;
+	}
+	get age(){
+		return this._age;
+	}
+	set age(age)
+	{
+		this._age = age;
+	}
+}
 
-function SportsCar(make, model, topSpeed) {}
+class Student extends Person {
+	study(){
+		console.log(`${this.name} is studying`)
+	}
+}
 
-// Do not change the code below
-window.Car = Car;
-window.SportsCar = SportsCar;
+class Teacher extends Person {
+	teach(){
+		console.log(`${this.name} is teaching`)
+	}
+}
+
+// Do not change the code below this line
+window.Person = Person;
+window.Student = Student;
+window.Teacher = Teacher;
